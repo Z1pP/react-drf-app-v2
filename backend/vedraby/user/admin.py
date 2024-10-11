9b337d312523885b57174f6527a97de9aacf21d5
Z1pP <maxm_99@mail.ru>
2024-10-12 01:22:41 +0300

    added CustomUserAdmin

--- backend/vedraby/user/admin.py
+++ backend/vedraby/user/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_active")
+    ordering = ("email",)
+    search_fields = ("email", "first_name", "last_name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Персональная информация", {"fields": ("first_name", "last_name")}),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Важные даты", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
