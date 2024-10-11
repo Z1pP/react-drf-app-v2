0a48214ef885e760056e03a77c58f3c51c0caa1c
Z1pP <maxm_99@mail.ru>
2024-10-12 02:02:05 +0300

    added sigral for creat profile

---
+++ backend/vedraby/user/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import CustomUser
+from vedraby.profiles.models import Profile
+
+
+@receiver(post_save, sender=CustomUser)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        Profile.save()
