0900956bb687bf6c53b7f6cee68c86ea12a89e0e
Z1pP <maxm_99@mail.ru>
2024-10-12 02:09:50 +0300

    update import path

--- backend/vedraby/user/signals.py
+++ backend/vedraby/user/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from profiles.models import Profile
 from .models import CustomUser
-from vedraby.profiles.models import Profile
 
 
 @receiver(post_save, sender=CustomUser)
