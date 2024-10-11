96c0a59cc0300f1a2d96b29de000ed7e2ab3fbb5
Z1pP <maxm_99@mail.ru>
2024-10-12 02:01:11 +0300

    added model for profile

--- backend/vedraby/profiles/models.py
+++ backend/vedraby/profiles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from vedraby.user.models import CustomUser
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name="profile"
+    )
+    location = models.CharField(
+        verbose_name="Местоположение", blank=True, null=True, max_length=100
+    )
+    phone_number = models.CharField(
+        verbose_name="Номер телефона", blank=True, null=True
+    )
+    avatar = models.ImageField("Аватар", upload_to="/avatars", blank=True, null=True)
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField("Дата обновления", auto_now=True)
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self) -> str:
+        return f"Профиль пользователя {self.user.email}"
