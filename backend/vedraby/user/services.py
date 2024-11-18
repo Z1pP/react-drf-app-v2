2a5fd479b7014232bfaca1f2b26b58cb0e7ce347
Z1pP <maxm_99@mail.ru>
2024-11-18 22:09:49 +0300

    added checking user password

--- backend/vedraby/user/services.py
+++ backend/vedraby/user/services.py
@@ -6,6 +6,13 @@ class UserService:
     def __init__(self, repository: UserRepository):
         self.repository = repository
 
+    def get_user_and_check_password(self, email: str, password: str) -> UserDTO | None:
+        user_db = self.repository.get_by_email(email=email)
+        if not user_db:
+            return None
+        if user_db.check_password(raw_password=password):
+            return user_db.to_dto()
+
     def get_all_users(self) -> list[UserDTO]:
         users_db = self.repository.all()
         return [user.to_dto() for user in users_db]
@@ -15,7 +22,9 @@ class UserService:
         return user_db.to_dto()
 
     def get_user_by_id(self, id: int) -> UserDTO:
-        user_db = self.repository.get(id=id)
+        user_db = self.repository.get_by_id(id=id)
+        if not user_db:
+            return None
         return user_db.to_dto()
 
     def update_user(self, user_id: int, user: UserDTO) -> UserDTO:
