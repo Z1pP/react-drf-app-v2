7aeda31801abc26a5e0eef8bacabe12945c8f782
Z1pP <maxm_99@mail.ru>
2024-11-18 22:10:06 +0300

    rename methods

--- backend/vedraby/user/repository.py
+++ backend/vedraby/user/repository.py
@@ -9,7 +9,11 @@ class BaseUserRepository(ABC):
         pass
 
     @abstractmethod
-    def get(self, id: int) -> CustomUser:
+    def get_by_id(self, id: int) -> CustomUser:
+        pass
+
+    @abstractmethod
+    def get_by_email(self, email: str) -> CustomUser:
         pass
 
     @abstractmethod
@@ -32,11 +36,20 @@ class UserRepository(BaseUserRepository):
     def create(self, email: str, password: str) -> CustomUser:
         return CustomUser.objects.create(email=email, password=password)
 
-    def get(self, id: int) -> CustomUser:
-        return CustomUser.objects.get(id=id)
+    def get_by_id(self, id: int) -> CustomUser | None:
+        try:
+            return CustomUser.objects.get(id=id)
+        except CustomUser.DoesNotExist:
+            return None
+
+    def get_by_email(self, email: str) -> CustomUser | None:
+        try:
+            return CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return None
 
     def update(self, id: int, **kwargs) -> CustomUser:
-        user = self.get(id)
+        user = self.get_by_id(id)
         for key, value in kwargs.items():
             if value is not None:
                 setattr(user, key, value)
