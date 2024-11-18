631d28db204d026cb3427a3dbbd199656a0cfd2a
Z1pP <maxm_99@mail.ru>
2024-11-18 22:04:51 +0300

    added auth router

--- backend/vedraby/apiNinja/handlers/__init__.py
+++ backend/vedraby/apiNinja/handlers/__init__.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
 
 from .user import router as user_router
+from .authentication import router as authentication_router
 
 api = NinjaAPI()
 api.add_router("users/", user_router, tags=["users"])
+api.add_router("auth/", authentication_router, tags=["authentication"])
